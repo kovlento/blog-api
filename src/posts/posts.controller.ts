8a6e425cd794f1a8e2751514e8e6ebb8d3e5053b
kovlento <bluskytee@163.com>
2019-11-15 15:00:28 +0800

    crud

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,62 @@
+import { Controller, Get, Post, Body, Query, Put, Param, Delete } from '@nestjs/common';
+import { ApiUseTags, ApiOperation, ApiModelProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator'
+import { InjectModel } from 'nestjs-typegoose';
+import {Post as PostSchema} from './post.model'
+import { ModelType } from '@typegoose/typegoose/lib/types';
+
+class CreatePostDto {
+  @ApiModelProperty({description:'帖子标题', example: '帖子标题1'})
+  @IsNotEmpty({message:'请填写标题'})
+  title: string;
+  @ApiModelProperty({description:'帖子内容', example: '帖子内容1'})
+  content: string;
+}
+
+@Controller('posts')
+@ApiUseTags('帖子')
+export class PostsController {
+  constructor(
+    @InjectModel(PostSchema) private readonly postModel: ModelType<PostSchema>
+  ){}
+
+
+  @Get()
+  @ApiOperation({ title: '显示帖子列表' })
+  async index() {
+    return await this.postModel.find()
+  }
+
+  @Post()
+  @ApiOperation({ title: '创建帖子' })
+  async create(@Body() createPostDto:CreatePostDto) {
+    await this.postModel.create(createPostDto)
+    return {
+      success: true,
+    };
+  }
+
+  @Get(':id')
+  @ApiOperation({ title: '帖子详情' })
+  async detail(@Param('id') id:string) {
+    return await this.postModel.findById(id)
+  }
+
+  @Put(':id')
+  @ApiOperation({ title: '编辑帖子' })
+  async update(@Body() createPostDto:CreatePostDto, @Param('id') id:string) {
+    await this.postModel.findByIdAndUpdate(id, createPostDto);
+    return {
+      success: true,
+    };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ title: '删除帖子' })
+  async remove(@Param('id') id:string){
+    await this.postModel.findByIdAndDelete(id);
+    return {
+      success:true
+    }
+  }
+}
